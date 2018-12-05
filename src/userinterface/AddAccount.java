8540b338765ec2df8c80ca3d554d655d26c50375
Cerelius <JasonLeeTrue@gmail.com>
2018-12-05 18:43:59 -0500

    Update AddAccount.java
    Built checks for the fields in addacount.java.  It should throw exceptions and print an error message to the user when there is improper input.

--- src/userinterface/AddAccount.java
+++ src/userinterface/AddAccount.java
@@ -11,6 +11,7 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import java.sql.*;
@@ -20,7 +21,7 @@ public class AddAccount extends BasicLayout implements ActionListener{
 	JLabel header = new JLabel("Add Account");
 	JButton cancel = new JButton("Cancel");
 	JButton save = new JButton("Save Account");
-	
+
 	JLabel acctNumLabel = new JLabel("Account Number:");
 	JLabel routNumLabel = new JLabel("Routing Number:");
 	JLabel bankLabel = new JLabel("Bank Name:");
@@ -35,7 +36,8 @@ public class AddAccount extends BasicLayout implements ActionListener{
 	JTextField balanceText = new JTextField(10);
 	JLabel [] labels = {acctNumLabel, routNumLabel, bankLabel, acctNameLabel, cardNumLabel, balanceLabel};
 	JTextField [] textFields = {acctNumText, routNumText, bankText, acctNameText, cardNumText, balanceText};
-	
+
+	// Build the interface.
 	public AddAccount(){
 		middle.setLayout(new GridBagLayout());
 		for (int i=0 ; i< labels.length ; i++){
@@ -61,47 +63,6 @@ public class AddAccount extends BasicLayout implements ActionListener{
 		save.addActionListener(this);
 	}
 
-	public void testDB() throws SQLException{
-		// this is a dummy method to show how to connect to the database and execute queries
-		Connection conn = get_connection();
-    	Statement stmt = conn.createStatement();
-    	String sql = "SELECT * FROM Users where Username = ?";
-    	PreparedStatement prepared_statement = conn.prepareStatement(sql);
-    	String username = "SuperKoolUser91";
-    	prepared_statement.setString(1, username);
-    
-    	ResultSet rs = prepared_statement.executeQuery();
-    	ResultSetMetaData rsmd = rs.getMetaData();
-    	
-    	int columnsNumber = rsmd.getColumnCount();
-    	
-    	while (rs.next()) 
-    	{
-    	    for (int i = 1; i <= columnsNumber; i++) 
-    	    {
-    	        if (i > 1) 
-    	        {
-    	        	System.out.print(",  ");
-    	        }
-    	        String columnValue = rs.getString(i);
-    	        System.out.print(columnValue + " " + rsmd.getColumnName(i));
-    	    }
-    	    System.out.println("");
-    	}
-    	conn.close();
-	}
-		
-	public void insertAccount(String values) throws SQLException{
-		// execute update to add account into database
-		Connection conn = get_connection();
-    	Statement stmt = conn.createStatement();
-    	String sql = "INSERT INTO UserAccounts (AccountNumber, RoutingNum, Bank_Name, Account_Title, UserName, CardNum, Balance)"
-    			+ "VALUES (" + values + ")";
-    	PreparedStatement prepared_statement = conn.prepareStatement(sql);
-    	prepared_statement.executeUpdate();
-    	conn.close();
-    	
-	}
 	@Override	
 	public void actionPerformed(ActionEvent e) {
 		JButton button = (JButton)e.getSource();
@@ -111,25 +72,190 @@ public class AddAccount extends BasicLayout implements ActionListener{
 			BudgetApplet.changeScreen("Accounts Summary");
 		}
 		//if save is pressed 
-		if (label == "Save Account"){
-			String acctNum = acctNumText.getText();
-			String routNum = routNumText.getText();
-			String bank = bankText.getText();
-			String acctName = acctNameText.getText();
-			String cardNum = cardNumText.getText();
-			String balance = balanceText.getText();
-			
-			// insert account into database using these values 
-			// still need to get userName from current user somehow 
-			String values =  "\"" + acctNum+ "\", \"" +	routNum+ "\", \"" + bank+ "\", \"" + acctName+ "\", \"" + "SuperKoolUser91"+ "\", \"" + cardNum+ "\", \"" + balance + "\"";
- 			System.out.println(values);
+		if (label == "Save Account"){	
+
 			try {
-				insertAccount(values);
+				insertAccount(buildNewAccountSignature());
 			} catch (SQLException e1) {
+				//This is a generic exception that indicates there was a problem with insertAccount.
 				e1.printStackTrace();
+				JOptionPane.showMessageDialog(null, "There was a problem with creating your account, please check your input and try again.");
+			} catch (InvalidAddAccountInputException e2) {
+				//This means that there was a problem with the input from the user.
+				e2.printImproperInput();
+				e2.showErrorMessage();
 			}
+
 			BudgetApplet.changeScreen("Accounts Summary");
 		}
-		
+
 	}
+	
+	public void insertAccount(String values) throws SQLException{
+		// execute update to add account into database
+		Connection conn = get_connection();
+		Statement stmt = conn.createStatement();
+		String sql = "INSERT INTO UserAccounts (AccountNumber, RoutingNum, Bank_Name, Account_Title, UserName, CardNum, Balance)"
+				+ "VALUES (" + values + ")";
+		PreparedStatement prepared_statement = conn.prepareStatement(sql);
+		prepared_statement.executeUpdate();
+		conn.close();
 	}
+
+	/** InvalidAddAccountInputException
+	 * This exception is thrown when there is invalid input in the 
+	 * 
+	 *
+	 */
+	public class InvalidAddAccountInputException extends Exception {
+		String cause;
+
+		public InvalidAddAccountInputException(String cause) {
+			this.cause = cause;
+		}
+
+		public void printImproperInput() {
+			System.out.println(cause);
+		}
+
+		public void showErrorMessage() {
+			JOptionPane.showMessageDialog(null, cause);
+		}
+	} 
+
+	/** buildNewAccountSignature
+	 * This will build the signature of the account to be inserted to the database
+	 * using the fields.
+	 * @return Signature containing the account's information.
+	 * @throws InvalidAddAccountInputException 
+	 */
+	public String buildNewAccountSignature() throws InvalidAddAccountInputException {
+		checkAddAccountInput();
+
+		String acctNum = acctNumText.getText();
+		String routNum = routNumText.getText();
+		String bank = bankText.getText();
+		String acctName = acctNameText.getText();
+		String cardNum = cardNumText.getText();
+		String balance = balanceText.getText();
+
+		String signature = "\"" + acctNum+ "\","
+				+ " \"" +	routNum+ "\","
+				+ " \"" + bank+ "\","
+				+ " \"" + acctName+ "\","
+				+ " \"" + "SuperKoolUser91"+ "\","
+				+ " \"" + cardNum+ "\","
+				+ " \"" + balance + "\"";
+
+		return signature;
+	}
+
+	/** checkInput
+	 * This method validates the input from the fields.
+	 * @param values - a string that is passed to the database that contains all of the query information.  
+	 * @throws inputInvalidException - There was a problem with the field information, the message defines what is wrong.  
+	 */
+	public void checkAddAccountInput() throws InvalidAddAccountInputException {
+		checkAccountNumber();
+		checkRoutingNumber();
+		checkBankName();
+		checkAccountName();
+		checkCardNumer();
+		checkBalance();
+	}
+
+	private void checkAccountNumber() throws InvalidAddAccountInputException {
+		// No non-numeric digits
+		// Must not be blank
+		String acctNum = acctNumText.getText();
+		if (acctNum == "")
+			throw new InvalidAddAccountInputException("Account Number cannot be blank");
+		else if (!isNumeric(acctNum))
+			throw new InvalidAddAccountInputException("Account Numer must be numeric");
+	}
+
+	private void checkRoutingNumber() throws InvalidAddAccountInputException{
+		// No non-numeric digits
+		// Must not be blank
+		String routNum = routNumText.getText();
+		if (routNum == "")
+			throw new InvalidAddAccountInputException("Account Number cannot be blank");
+		else if (isNumeric(routNum))
+			throw new InvalidAddAccountInputException("Account Numer must be numeric");	
+	}
+
+	private void checkBankName() throws InvalidAddAccountInputException {
+		// Must not be blank
+		if (bankText.getText() == "")
+			throw new InvalidAddAccountInputException("Bank Name cannot be blank");
+	}
+
+	private void checkAccountName() throws InvalidAddAccountInputException {
+		// Must not be blank		
+		if (acctNameText.getText() == "")
+			throw new InvalidAddAccountInputException("Account Name cannot be blank");
+	}
+
+	private void checkCardNumer() throws InvalidAddAccountInputException {
+		// cannot be blank
+		// Must be numeric
+		String cardNum = cardNumText.getText();
+		if (cardNum == "")
+			throw new InvalidAddAccountInputException("Card Number cannot be blank.");
+		else if (!isNumeric(cardNum))
+			throw new InvalidAddAccountInputException("Card Number must be numeric.");
+	}
+
+	private void checkBalance() throws InvalidAddAccountInputException {
+		// Cannot be blank
+		// Must be numeric
+		// Must be non-negative
+		String balance = balanceText.getText();
+		if (balance == "")
+			throw new InvalidAddAccountInputException("balance cannot be blank.");
+		else if (!isNumeric(balance))
+			throw new InvalidAddAccountInputException("balance has to be numeric.");
+		else if (Integer.parseInt(balance) < 0)
+			throw new InvalidAddAccountInputException("balance cannot be negative.");
+	}
+
+	// Checks if a string is numeric or not.
+	private boolean isNumeric(String strNum) {
+		try {
+			double d = Double.parseDouble(strNum);
+		} catch (NumberFormatException | NullPointerException nfe) {
+			return false;
+		}
+		return true;
+	}
+	
+//	public void testDB() throws SQLException{
+//	// this is a dummy method to show how to connect to the database and execute queries
+//	Connection conn = get_connection();
+//	Statement stmt = conn.createStatement();
+//	String sql = "SELECT * FROM Users where Username = ?";
+//	PreparedStatement prepared_statement = conn.prepareStatement(sql);
+//	String username = "SuperKoolUser91";
+//	prepared_statement.setString(1, username);
+//
+//	ResultSet rs = prepared_statement.executeQuery();
+//	ResultSetMetaData rsmd = rs.getMetaData();
+//
+//	int columnsNumber = rsmd.getColumnCount();
+//
+//	while (rs.next()) 
+//	{
+//		for (int i = 1; i <= columnsNumber; i++) 
+//		{
+//			if (i > 1) 
+//			{
+//				System.out.print(",  ");
+//			}
+//			String columnValue = rs.getString(i);
+//			System.out.print(columnValue + " " + rsmd.getColumnName(i));
+//		}
+//		System.out.println("");
+//	}
+//	conn.close();
+//}
+}
