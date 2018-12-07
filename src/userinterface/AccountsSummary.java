4e6adae062b262c80f1af3710ed81a9e3afe84aa
torip1030 <torip1030@gmail.com>
2018-12-06 20:05:46 -0500

    AcctSum and TransSum: show from db, delete button works

--- src/userinterface/AccountsSummary.java
+++ src/userinterface/AccountsSummary.java
@@ -8,24 +8,94 @@ import java.awt.Label;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 
 public class AccountsSummary extends BasicLayout implements ActionListener{
 
 	JLabel header = new JLabel("Payment Accounts");
 	JButton addAcct = new JButton("Add Account");
 	JButton budSum = new JButton("Budget Summary");
+	ArrayList<Account> acct_list;
 	
 	public AccountsSummary(){
 		top.add(header);
-		// put this in loop and do it for each account in DB
-			JLabel acct_info = new JLabel("Savings	$2000");
+		middle.setLayout(new GridBagLayout());
+		try {
+			acct_list = getAccounts();
+		} catch (SQLException e1) {
+				e1.printStackTrace();
+		}
+		createMiddle(acct_list);
+		bottom.setLayout(new GridLayout(1,2));
+		bottom.add(budSum);
+		budSum.addActionListener(this);
+		bottom.add(addAcct);
+		addAcct.addActionListener(this);
+	}
+	
+	public void createMiddle( ArrayList<Account> acct_list){
+		int row = 1;
+		for (Account account : acct_list){
+			String acct_number = account.getNum();
+			String rout_number = account.getRout();
+			String acct_string = account.getInfo();
+			// put this in loop and do it for each account in DB
+			JLabel acct_info = new JLabel(acct_string);
 			JButton edit_button = new JButton("Edit");
 			JButton delete_button = new JButton("Delete");
-			edit_button.addActionListener(this);
+			edit_button.addActionListener(new ActionListener(){
+				public void actionPerformed(ActionEvent e){
+					//method to populate add account screen with this account's data
+					//AddAccount.populate();
+					BudgetApplet.changeScreen("Add Account");
+					}
+				/*
+				 * String acct_number = account.getNum();
+				 * String rout_number = account.getRout();
+				 * populate(acct_number, rout_number)
+				 * query for account info from id
+				 * set text fields to info from query
+				 * 
+				 */
+				
+			});
+			
+			delete_button.addActionListener(new ActionListener(){
+				ArrayList<Account> acc;
+				public void actionPerformed(ActionEvent e){
+					//ask user in popup if they really want to delete this account
+					//method to delete this account from the database
+					try {
+						deleteAccount(acct_number,rout_number);
+						deleteLinkedTrans(acct_number,rout_number);
+						//send user alert that account was deleted
+						JOptionPane.showMessageDialog(null,"Account: "+ acct_string + "\n and linked transactions deleted ");
+						middle.removeAll();
+						try {
+							acc = getAccounts();
+						} catch (SQLException e1) {
+								e1.printStackTrace();
+						}
+						createMiddle(acc);
+					} catch (SQLException e1) {
+						e1.printStackTrace();
+						JOptionPane.showMessageDialog(null,"Problem deleteing account from the database");
+					}
+					
+					}
+			});
 			JPanel acct = new JPanel();
 			acct.setLayout(new GridBagLayout());
 			GridBagConstraints c = new GridBagConstraints();
@@ -46,16 +116,70 @@ public class AccountsSummary extends BasicLayout implements ActionListener{
 			e.gridy = 1;
 			e.gridwidth = 1;
 			acct.add(delete_button, e);
-			middle.add(acct);
-		
-		bottom.setLayout(new GridLayout(1,2));
-		bottom.add(budSum);
-		budSum.addActionListener(this);
-		bottom.add(addAcct);
-		addAcct.addActionListener(this);
-		
+			
+			GridBagConstraints f = new GridBagConstraints();
+			f.fill = GridBagConstraints.HORIZONTAL;
+			f.gridx = 1;
+			f.gridy = row;
+			f.gridwidth = 1;
+			middle.add(acct,f);
+			row++;
+		}
+		}
+	
+	public ArrayList<Account> getAccounts() throws SQLException{
+		Connection conn = get_connection();
+		Statement stmt = conn.createStatement();
+		String sql = "SELECT * FROM UserAccounts WHERE Username = \"SuperKoolUser91\";";
+    	PreparedStatement prepared_statement = conn.prepareStatement(sql);
+    	ResultSet rs = prepared_statement.executeQuery();
+    	ResultSetMetaData rsmd = rs.getMetaData();
+    	
+    	ArrayList <Account> accts = new ArrayList<>();
+    	
+    	while (rs.next()) {
+    		String acct_num = rs.getString(1);
+    		String rout_num = rs.getString(2);
+    		// account info =  account name and bank name 
+    		String acct_info = rs.getString(4)+ " - "+ rs.getString(3);
+    		Account result = new Account(acct_num, rout_num, acct_info);
+    		accts.add(result);
+    	}
+    	conn.close();
+    	return accts;
+	}
+	
+	public void deleteAccount(String acct_num, String rout_num) throws SQLException{
+		// execute update to delete account from database
+		Connection conn = get_connection();
+    	Statement stmt = conn.createStatement();
+    	//insert query here
+    	String sql = "DELETE FROM UserAccounts WHERE AccountNumber = ? AND RoutingNum = ?;";
+    	PreparedStatement prepared_statement = conn.prepareStatement(sql);
+    	prepared_statement.setString(1, acct_num);
+    	prepared_statement.setString(2, rout_num);
+    	prepared_statement.executeUpdate();
+    	conn.close();
 	}
 
+	public void deleteLinkedTrans(String acct_num, String rout_num) throws SQLException{
+		Connection conn = get_connection();
+		String sql = "SELECT AccountNumber from UserAccounts WHERE AccountNumber = ? AND RoutingNum = ?; ";
+    	PreparedStatement prepared_statement = conn.prepareStatement(sql);
+    	prepared_statement.setString(1, acct_num);
+    	prepared_statement.setString(2, rout_num);
+    	ResultSet rs = prepared_statement.executeQuery();
+    	ResultSetMetaData rsmd = rs.getMetaData();
+    	int columnsNumber = rsmd.getColumnCount();
+    	
+    	while (rs.next()) {
+    	    for (int i = 1; i <= columnsNumber; i++) 
+    	    {
+    		System.out.println(rs.getString(i));
+    	    }
+    	}
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		JButton button = (JButton)e.getSource();
@@ -67,6 +191,28 @@ public class AccountsSummary extends BasicLayout implements ActionListener{
 		//if addAcct is pressed 
 		if (label == "Add Account"){
 			BudgetApplet.changeScreen("Add Account");
+		}
+		
+	}
+	
+	public class Account{
+		private String account_number;
+		private String routing_number;
+		private String acct_string;
+		
+		public Account(String acct_num, String rout_num, String acct_str){
+			this.account_number = acct_num;
+			this.routing_number = rout_num;
+			this.acct_string = acct_str;
+		}
+		public String getNum(){
+			return account_number;
+		}
+		public String getRout(){
+			return routing_number;
+		}
+		public String getInfo(){
+			return acct_string;
 		}
 		
 	}
--- src/userinterface/TransactionSummary.java
+++ src/userinterface/TransactionSummary.java
@@ -7,23 +7,78 @@ import java.awt.GridLayout;
 import java.awt.Label;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+
+import userinterface.AccountsSummary.Account;
 public class TransactionSummary extends BasicLayout implements ActionListener {
 
 	JLabel header = new JLabel("Transactions");
 	JButton addTrans = new JButton("Add Transaction");
 	JButton budSum = new JButton("Budget Summary");
+	ArrayList<Transaction> trans_list;
 	
 	public TransactionSummary(){
 		top.add(header);
-		// loop to do this for each transaction 
-			JLabel trans_info = new JLabel("11/11/18 Chipotle $21.25 Credit");
+		middle.setLayout(new GridBagLayout());
+		try {
+			trans_list = getTransactions();
+		} catch (SQLException e1) {
+				e1.printStackTrace();
+		}
+		createMiddle(trans_list);
+	
+		bottom.add(budSum);
+		budSum.addActionListener(this);
+		bottom.add(addTrans);	
+		addTrans.addActionListener(this);
+	}
+	
+	
+	public void createMiddle(ArrayList<Transaction> trans_list ){
+		int row = 1;
+		for (Transaction tran: trans_list){ 
+			JLabel trans_info = new JLabel(tran.getTransString());
 			JButton edit_button = new JButton("Edit");
 			JButton delete_button = new JButton("Delete");
-			edit_button.addActionListener(this);
+			edit_button.addActionListener(new ActionListener(){
+				public void actionPerformed(ActionEvent e){
+					//method to populate add transaction screen with this transaction's data
+					//AddTransaction.populate();
+					BudgetApplet.changeScreen("Add Transaction");
+					}
+			});
+			delete_button.addActionListener(new ActionListener(){
+				ArrayList<Transaction> trans_list;
+				public void actionPerformed(ActionEvent e){
+					//ask user in popup if they really want to delete this transaction
+					try {
+						deleteTransaction(tran.getCreditID());
+						JOptionPane.showMessageDialog(null,"Transaction: "+ tran.getTransString() + " deleted ");		
+						middle.removeAll();
+						try {
+							trans_list = getTransactions();
+						} catch (SQLException e1) {
+								e1.printStackTrace();
+						}
+						createMiddle(trans_list);
+						
+					} catch (SQLException e1) {
+						e1.printStackTrace();
+						JOptionPane.showMessageDialog(null,"Problem deleting transaction from the database");
+					}
+				}
+			});
 			JPanel trans = new JPanel();
 			trans.setLayout(new GridBagLayout());
 			GridBagConstraints c = new GridBagConstraints();
@@ -44,13 +99,57 @@ public class TransactionSummary extends BasicLayout implements ActionListener {
 			e.gridy = 1;
 			e.gridwidth = 1;
 			trans.add(delete_button, e);
-		middle.add(trans);
-		bottom.add(budSum);
-		budSum.addActionListener(this);
-		bottom.add(addTrans);	
-		addTrans.addActionListener(this);
+			
+			GridBagConstraints f = new GridBagConstraints();
+			f.fill = GridBagConstraints.HORIZONTAL;
+			f.gridx = 1;
+			f.gridy = row;
+			f.gridwidth = 1;
+			middle.add(trans,f);
+			row++;
+			}
+			
 	}
-
+	
+	public ArrayList<Transaction> getTransactions() throws SQLException{
+		Connection conn = get_connection();
+		Statement stmt = conn.createStatement();
+		String sql = "SELECT CreditID, Title, Amount, Category, DateCreated FROM Credit WHERE Username = \"NotSoKoolUser11\" ORDER BY DateCreated ASC;"; 
+    	PreparedStatement prepared_statement = conn.prepareStatement(sql);
+    	ResultSet rs = prepared_statement.executeQuery();
+    	ResultSetMetaData rsmd = rs.getMetaData();
+    	
+    	ArrayList <Transaction> accts = new ArrayList<>();
+    	
+    	while (rs.next()) {
+    		
+    		String creditID = rs.getString(1);
+    		String title = rs.getString(2);
+    		String amount = rs.getString(3);
+    		String category = rs.getString(4);
+    		String date = rs.getString(5);
+    		// transaction info =  title,amount,category,date
+    		String transaction_info = date + "   "+ title + "   $" + amount  + "   " + category;
+    		Transaction result = new Transaction(creditID, transaction_info);
+    		accts.add(result);
+    	}
+    	conn.close();
+    	return accts;
+	}
+	
+	public void deleteTransaction(String creditID) throws SQLException{
+		// execute update to delete account from database
+		Connection conn = get_connection();
+    	Statement stmt = conn.createStatement();
+    	//insert query here
+    	String sql = "DELETE FROM Credit WHERE CreditID = ? AND Username = ?;";
+    	PreparedStatement prepared_statement = conn.prepareStatement(sql);
+    	prepared_statement.setString(1, creditID);
+    	prepared_statement.setString(2, "NotSoKoolUser11");
+    	prepared_statement.executeUpdate();
+    	conn.close();
+	}
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		JButton button = (JButton)e.getSource();
@@ -63,13 +162,20 @@ public class TransactionSummary extends BasicLayout implements ActionListener {
 		else if (label == "Add Transaction"){
 			BudgetApplet.changeScreen("Add Transaction");
 		}
-		else if (label == "Edit"){
-			//other if statement to get info about transaction
-			BudgetApplet.changeScreen("Add Transaction");
-			//other stuff to make it edit not add
+	}
+	public class Transaction{
+		private String creditID;
+		private String trans_string;
+		
+		public Transaction(String credit, String trans_str){
+			this.creditID = credit;
+			this.trans_string = trans_str;
 		}
-		else if (label == "Delete"){
-			//do stuff, maybe ask if they are sure that they want to delete this transaction
+		public String getCreditID(){
+			return creditID;
+		}
+		public String getTransString(){
+			return trans_string;
 		}
 		
 	}
